6aeda4999ee869a44bca4f4449cc1aa086182dd0
Shaif Azad <shaif.rahi@northsouth.edu>
2021-07-27 16:37:54 +0600

    zoom, center and height changed

--- react-web-app/view/src/components/live-tracking/LiveTracking.js
+++ react-web-app/view/src/components/live-tracking/LiveTracking.js
@@ -26,7 +26,7 @@ const LiveTracking = () => {
             {devices.length>0 && <CContainer>
                 <CRow>
                     <div>
-                    <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+                    <MapContainer center={[23.6850, 90.3563]} zoom={8} scrollWheelZoom={false}>
                         <TileLayer
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
